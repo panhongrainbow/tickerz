146715dffc53d0ac04aafc73f69d92728a884bbf
PanHongRainbow <panhongrainbow@gmail.com>
2023-03-18 16:18:22 +0800

    [base package] is a time-related package that includes constants, error types, structs, and functions for manipulating time and determining time types.
    1. Defines some constants, including date and time formats, time zones, and so on.
    2. Defines an error type Error and implements the Error() method.
    3. Defines a struct Base, which represents basic timestamp, time zone, and options.
    4. Defines a struct Opts, which represents some options, including base time, time zone, repeat, duration,
    5. base list, and so on.
    6. Defines a struct OffOpts, which represents some closed options, including every day off, Monday to Sunday off, and so on.
    7. Defines a function init(), which is used to initialize the default time zone.
    8. Defines a variable defaultTimeLocation, which represents the default time zone.
    9. Defines a function TimeType(input string), which is used to determine the input time type.
    10. Defines a method called CheckOpts, which takes an Opts parameter and returns an error. The main purpose of this method is to check whether the parameters in Opts meet the requirements, and return the corresponding error if they do not.

---
+++ base/base.go
@@ -0,0 +1,280 @@
+package base
+
+import (
+	"errors"
+	"log"
+	"time"
+)
+
+// default time format string
+const (
+	DefaultDateFormatStr     = "2006-1-2"
+	DefaultTimeFormatStr     = "15:4:5"
+	DefaultDateTimeFormatStr = "2006-1-2 15:4:5"
+	DefaultTimeZone          = "Asia/Shanghai"
+)
+
+// time formats
+const (
+	DateFormat uint = iota + 1
+	TimeFormat
+	DatetimeFormat
+)
+
+const (
+	ErrUnSupportedTimeFormat      = Error("unsupported time format")
+	ErrUnSupportedLocation        = Error("unsupported location")
+	ErrUnSupportedBeginTimeFormat = Error("unsupported begin time format")
+	ErrUnSupportedEndTimeFormat   = Error("unsupported end time format")
+	ErrUnsupBasetime              = Error("unsupported basetime")
+	ErrNegativeDuration           = Error("negative time duration")
+	ErrUnSupportedBaseList        = Error("unsuported base list")
+	ErrBaseListOverlaps           = Error("base list overlaps")
+	ErrIncorrectBaseListOrder     = Error("incorrect base list order")
+	ErrBaseListDifferentTypes     = Error("base list contains products of different types")
+	ErrBeginEndTimeTypeNotEqual   = Error("begin time's type is not the same as end time's")
+	ErrIncorrectBeginEndTimeOrder = Error("incorrect begin end time order")
+)
+
+type Error string
+
+func (e Error) Error() string {
+	return string(e)
+}
+
+type Base struct {
+	BaseStamp    int64
+	BaseLocation *time.Location
+	Opts         Opts
+	OffOpts      OffOpts
+	BeginStamp   int64
+	EndStamp     int64
+	BaseListType uint
+}
+
+type Opts struct {
+	BaseTime  string // dateTime or time
+	Location  string
+	Repeat    bool
+	Duration  time.Duration
+	BaseList  []string
+	BeginTime string
+	EndTime   string
+}
+
+type OffOpts struct {
+	EveryDayOff  bool
+	MondayOff    bool
+	TuesdayOff   bool
+	WednesdayOff bool
+	ThursdayOff  bool
+	FridayOff    bool
+	SaturdayOff  bool
+	SundayOff    bool
+}
+
+func init() {
+	var err error
+	defaultTimeLocation, err = time.LoadLocation("Asia/Shanghai")
+	if err != nil {
+		log.Fatal(errors.New("default timeLocation error"))
+	}
+}
+
+var defaultTimeLocation *time.Location
+
+func TimeType(input string) (output uint, err error) {
+	if _, err = time.Parse(DefaultDateFormatStr, input); err == nil {
+		output = DateFormat
+		return
+	}
+	if _, err = time.Parse(DefaultTimeFormatStr, input); err == nil {
+		output = TimeFormat
+		return
+	}
+	if _, err = time.Parse(DefaultDateTimeFormatStr, input); err == nil {
+		output = DatetimeFormat
+		return
+	}
+
+	err = ErrUnSupportedTimeFormat
+	return
+}
+
+func TimeValue(input string, location *time.Location) (output int64, err error) {
+	if location == nil {
+		location = defaultTimeLocation
+	}
+	t, err := time.ParseInLocation(DefaultDateTimeFormatStr, input, location)
+	if err != nil {
+		return -1, errors.New("not datetime type")
+	}
+	output = t.Unix()
+	return
+}
+
+func (receive Opts) CheckOpts() (err error) {
+	// check base time
+	var tp uint
+	tp, err = TimeType(receive.BaseTime)
+	if err == ErrUnSupportedTimeFormat {
+		return
+	}
+	if tp != TimeFormat && tp != DatetimeFormat {
+		err = ErrUnsupBasetime
+		return
+	}
+
+	// check location
+	if receive.Location == "" {
+		receive.Location = DefaultTimeZone
+	}
+	_, err = time.LoadLocation(receive.Location)
+	if err != nil {
+		err = ErrUnSupportedLocation
+		return
+	}
+
+	// check duration
+	if receive.Duration.Nanoseconds() < 0 {
+		err = ErrNegativeDuration
+		return
+	}
+
+	// check base list
+	now := time.Now()
+	date := now.Format("2006-1-2")
+	var previous time.Time
+	var previousType uint
+	for i := 0; i < len(receive.BaseList); i++ {
+		tp, err = TimeType(receive.BaseList[i])
+		if err != nil {
+			return
+		}
+		if tp != TimeFormat && tp != DatetimeFormat {
+			err = ErrUnSupportedBaseList
+			return
+		}
+
+		// make new datetime string
+		var datetime string
+		if tp == TimeFormat {
+			datetime = date + " " + receive.BaseList[i]
+		}
+		if tp == DatetimeFormat {
+			datetime = receive.BaseList[i]
+		}
+
+		// check if this base list contains products of different types？
+		if previousType == 0 {
+			previousType = tp
+		} else if previousType != 0 &&
+			previousType != tp {
+			err = ErrBaseListDifferentTypes
+			return
+		}
+
+		// check base list order
+		var tmp time.Time
+		tmp, err = time.Parse(DefaultDateTimeFormatStr, datetime)
+		if err != nil {
+			// return the original error message
+			return
+		}
+		if !previous.Before(tmp) {
+			err = ErrIncorrectBaseListOrder
+			return
+		} else if previous.Before(tmp) {
+			previous = tmp
+		}
+	}
+
+	// check begin time type
+	var beginTp uint
+	if receive.BeginTime != "" {
+		beginTp, err = TimeType(receive.BeginTime)
+		if err != nil {
+			err = ErrUnSupportedBeginTimeFormat
+			return
+		}
+		if beginTp != TimeFormat && beginTp != DatetimeFormat {
+			err = ErrUnSupportedBeginTimeFormat
+			return
+		}
+	}
+
+	// check end time type
+	var endTp uint
+	if receive.EndTime != "" {
+		endTp, err = TimeType(receive.EndTime)
+		if err != nil {
+			err = ErrUnSupportedEndTimeFormat
+			return
+		}
+		if endTp != TimeFormat && endTp != DatetimeFormat {
+			err = ErrUnSupportedEndTimeFormat
+			return
+		}
+	}
+
+	// receive.BeginTime's type must be the same as receive.EndTime's
+	if beginTp != endTp {
+		err = ErrBeginEndTimeTypeNotEqual
+		return
+	}
+
+	// check begin time and end time order
+	var begintimeStr string
+	var beginTime time.Time
+	if receive.BeginTime != "" {
+		if beginTp == TimeFormat {
+			begintimeStr = date + " " + receive.BeginTime
+		}
+		if beginTp == DatetimeFormat {
+			begintimeStr = receive.BeginTime
+		}
+		beginTime, err = time.Parse(DefaultDateTimeFormatStr, begintimeStr)
+		if err != nil {
+			// return the original error message
+			return
+		}
+	}
+
+	var endtimeStr string
+	var endTime time.Time
+	if receive.EndTime != "" {
+		if endTp == TimeFormat {
+			endtimeStr = date + " " + receive.EndTime
+		}
+		if endTp == DatetimeFormat {
+			endtimeStr = receive.EndTime
+		}
+		endTime, err = time.Parse(DefaultDateTimeFormatStr, endtimeStr)
+		if err != nil {
+			// return the original error message
+			return
+		}
+	}
+
+	if receive.BeginTime != "" && receive.EndTime != "" {
+		if !beginTime.Before(endTime) {
+			err = ErrIncorrectBeginEndTimeOrder
+			return
+		}
+	}
+
+	return
+}
+
+/*func _mustBeTimeFormatOrDatetimeFormat(input string) (err error) {
+	var tp uint
+	tp, err = TimeType(input)
+	if err == ErrUnSupportedTimeFormat {
+		return
+	}
+	if tp != TimeFormat && tp != DatetimeFormat {
+		err = ErrUnsupBasetime
+		return
+	}
+	return
+}*/
